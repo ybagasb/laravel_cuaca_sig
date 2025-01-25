abf460ca0072cf5f938b14fe756a69ee991c32b1
Yoga Bagas Budhiman <91241450+ybagasb@users.noreply.github.com>
2025-01-25 12:02:06 +0700

    Update tentang.blade.php

--- resources/views/tentang.blade.php
+++ resources/views/tentang.blade.php
@@ -74,7 +74,7 @@
         <div class="row">
             <div class="col-sm-6 mb-3 mb-sm-0">
                 <div class="card">
-                <img src="https://i.im.ge/2025/01/24/HoBjSS.2.png" class="card-img-top img-fluid" alt="...">
+                <img src="https://i.im.ge/2025/01/25/HXislW.2.webp" class="card-img-top img-fluid" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">Yoga Bagas Budhiman</h5>
                     <p class="card-text">NIM: 0110121086</p>
@@ -84,7 +84,7 @@
             </div>
             <div class="col-sm-6">
                 <div class="card">
-                <img src="https://i.im.ge/2025/01/24/HoBrip.1.png" class="card-img-top img-fluid" alt="...">
+                <img src="https://i.im.ge/2025/01/25/HXia00.1.webp" class="card-img-top img-fluid" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">Raihan Muzakki Nurhadi</h5>
                     <p class="card-text">NIM: 0110221163</p>
